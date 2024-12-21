e1a2a28867bde9bc7c05c35d9aabb555f4a517ec
crystalz05 <mikecrystalz05@gmail.com>
2024-12-21 21:51:13 +0100

    Updated submit pop up

--- src/assets/components/Test.js
+++ src/assets/components/Test.js
@@ -289,33 +289,33 @@ function Test() {
                   </div>
                 ))}
               </div>
-              {showPopUp && (
-                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-                  <div className="bg-white p-8 rounded-lg">
-                    <p className="text-lg font-semibold mb-4">
-                      Are you ready to submit
-                    </p>
-                    <div className="flex justify-end">
-                      <button
-                        onClick={closeModal}
-                        className="text-gray-500 hover:text-gray-700 mr-4"
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        // onClick={() => navigate("/completed")}
-                        onClick={handlingSubmit}
-                        className="bg-red-600 text-white px-4 py-2 rounded-full"
-                      >
-                        Confirm
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              )}
             </div>
           </div>
         </div>
+        {showPopUp && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-8 rounded-lg">
+              <p className="text-lg font-semibold mb-4">
+                Are you ready to submit
+              </p>
+              <div className="flex justify-end">
+                <button
+                  onClick={closeModal}
+                  className="text-gray-500 hover:text-gray-700 mr-4"
+                >
+                  Cancel
+                </button>
+                <button
+                  // onClick={() => navigate("/completed")}
+                  onClick={handlingSubmit}
+                  className="bg-red-600 text-white px-4 py-2 rounded-full"
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
